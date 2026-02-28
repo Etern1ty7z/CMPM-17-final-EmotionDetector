ebbb361475e9426f08ef3f937b93048d4b4504a4
Ewigkeit1203 <etern1ty061203@gmail.com>
2026-02-28 14:08:17 -0800

    Milestone 1 for Final

---
+++ final.py
@@ -0,0 +1,58 @@
+import os
+import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import v2 
+import numpy as np
+
+#-------------------------------------DATA Augumentation-----------------------------------------------------
+train_transform = v2.Compose([
+    v2.ToTensor(),
+    v2.RandomResizedCrop(48*48),
+    v2.RandomHorizontalFlip(0.3),
+    v2.Grayscale(num_output_channels=1),
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True) 
+])
+
+test_transform = v2.Compose([
+    v2.ToTensor(),
+    v2.RandomResizedCrop(48*48),
+    v2.Grayscale(num_output_channels=1),
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True)
+])
+
+'''
+Cannot use v2.ToPILImage():
+
+UserWarning: The transform `ToTensor()` is deprecated and will be removed in a future release.
+Instead, please use `v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])`.
+Output is equivalent up to float precision.  
+warnings.warn(
+'''
+#----------------------------------Creating DataLoader-------------------------------------------
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+root = os.path.join(base_path, 'data', 'data_organized')
+
+train_dataset = datasets.ImageFolder(os.path.join(root, 'train'), transform=train_transform)
+test_dataset = datasets.ImageFolder(os.path.join(root, 'test'), transform=test_transform)
+val_dataset = datasets.ImageFolder(os.path.join(root, 'val'), transform=test_transform)
+
+train_loader = DataLoader(train_dataset, batch_size=100, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+#---------------------------Display 100 images----------------------------------------------
+images, labels = next(iter(train_loader)) 
+plt.figure(figsize=(15, 15))
+for i in range(100):
+    plt.subplot(10, 10, i + 1)
+    plt.imshow(images[i].squeeze(), cmap='gray') 
+    plt.title(train_dataset.classes[labels[i]], fontsize=8)
+    plt.axis('off')
+plt.tight_layout()
+plt.show()
